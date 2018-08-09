be4020fa24ad9007e6b9abb3dccadf2cb1f7bec8
Rob Smith <merlin@sleepingkingstudios.com>
2018-08-09 16:43:52 -0400

    Implement Entities::RoomExit::Contract.

--- lib/explorer/entities/room_exit.rb
+++ lib/explorer/entities/room_exit.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'bronze/contracts/contract'
 require 'bronze/entities/entity'
 
 require 'explorer/entities'
@@ -9,6 +10,12 @@ module Explorer::Entities
   # ending nodes and contains information about traversing the edge, such as the
   # direction of travel in game space.
   class RoomExit < Bronze::Entities::Entity
+    # Default contract for validating RoomExit instances.
+    class Contract < Bronze::Contracts::Contract
+      constrain :origin, present: true
+      constrain :target, present: true
+    end
+
     attribute :direction, String
 
     references_one :origin,
